2fbeb598e44440ede8fd4334deb2fe34290cdf43
Carlos Mario <cmescandonpardo@gmail.com>
2022-11-21 10:09:44 -0500

    ESP-NOW Working on ESP32 as Server

--- espnow_server/espnow_server.ino
+++ espnow_server/espnow_server.ino
@@ -1,41 +1,66 @@
-#include <ESP8266WiFi.h>
-#include <espnow.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-// Structure to keep values
-typedef struct struct_message {
-  float temperature;
-  float humidity;
-};
+//#include <ESP8266WiFi.h>
+//#include <espnow.h>
 
-// Create a struct_message called myData
-struct_message myData;
+#define CHANNEL 1
 
-// callback function executed when data is received
-void OnRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Temperature: ");
-  Serial.println(myData.temperature);
-  Serial.print("Humidity: ");
-  Serial.println(myData.humidity);
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == 0) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  String Prefix = "Slave:";
+  String Mac = WiFi.macAddress();
+  String SSID = Prefix + Mac;
+  String Password = "123456789";
+  bool result = WiFi.softAP(SSID.c_str(), Password.c_str(), CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
 }
 
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  // Init ESP-NOW
-  if (esp_now_init() != 0) {
-    Serial.println("There was an error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once the ESP-Now protocol is initialized, we will register the callback function
-  // to be able to react when a package arrives in near to real time without pooling every loop.
-  esp_now_register_recv_cb(OnRecv);
+   Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
 }
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println(""); 
+}
+
 void loop() {
+  // Chill
 }
